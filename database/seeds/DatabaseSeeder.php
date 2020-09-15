86003b732826b12ac5dda1e5ec8d1aee7cb58c29
Ubuntu <ubuntu@ip-172-31-40-44.us-east-2.compute.internal>
2020-09-15 12:25:16 +0000

    Seeding the DB with fake data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+        factory(App\Model\Product::class,50)->create();
+        factory(App\Model\Review::class,300)->create();
+        
+        
     }
 }
